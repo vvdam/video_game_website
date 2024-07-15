c3ed07439a0bc91c3bbeffdf792e9d4acf3a4e86
vvdam <maaalouladam@gmail.com>
2024-07-15 23:49:30 +0200

    multiple page  added

--- javascript.js
+++ javascript.js
@@ -1,64 +1,48 @@
+let page = document.querySelector(".page");
+let result;
+let pages = 1;
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
 
-let page = document.querySelector('.page')
-let result
+resultashow();
 
-fetch('https://api.rawg.io/api/games?key=7c52816cd5364d64abce2ea208717ef8')
-    .then(response => response.json())
-    .then(img => {
-        let result = img.results
-        console.log(result)
-        result.forEach(element => {
-            const div = document.createElement("div")
-            div.classList.add("div")
-            const span= document.createElement("p")
-            span.classList.add("txt")
-            //const spann= document.createElement("p")
-            //spann.classList.add("txt2")
-            const img = document.createElement("img")
-            img.classList.add("imgg")
-            span.innerText = element.name
-            img.src = element.background_image
-            //spann.innerText=element.platforms.name
-            div.appendChild(img)
-            div.appendChild(span)
-            //div.appendChild(spann)
-            page.appendChild(div)
-        });
-    })
+next.addEventListener("click", function (e) {
+    pages++;
+    resultashow();
+    console.log(pages);
+});
+prev.addEventListener("click", function (e) {
+    if (pages > 1) {
+        pages--;
+        resultashow();
+    }
+});
 
+function resultashow() {
+    fetch(
+        `https://api.rawg.io/api/games?key=7c52816cd5364d64abce2ea208717ef8&page_size=100&page=${pages}`
+    )
+        .then((response) => response.json())
+        .then((img) => {
+            let result = img.results;
+            console.log(result);
 
-//function blink() {
-//    let txt = document.getElementById('titre');
-//    setInterval(function() {
-//    txt.style.display = (txt.style.display == 'none' ? '' : 'none');
-//}, 500);
+            // Effacer le contenu précédent
+            page.innerHTML = "";
 
-// for (let pas = 0; pas < 21; pas++) {
-    
-//     fetch('https://api.rawg.io/api/games?key=7c52816cd5364d64abce2ea208717ef8') 
-//     .then(response => response.json())
-    
-//     .then(data =>{
-        
-//         document.getElementById(`lop${pas}`).src=data.results[pas].background_image}) 
-//     .catch(error => console.error(error)) 
-
-
-// }
-
-// for (let pas = 0; pas < 21; pas++) {
-    
-//     fetch('https://api.rawg.io/api/platformes?key=7c52816cd5364d64abce2ea208717ef8') 
-//     .then(response => response.json())
-    
-//     .then(data =>{
-        
-//         document.getElementById(`name${pas}`).innerText=data.results[pas].name}) 
-//     .catch(error => console.error(error)) 
-
-
-// }
-
-
-
-// console.log(b)
+            result.forEach((element) => {
+                const div = document.createElement("div");
+                div.classList.add("div");
+                const span = document.createElement("p");
+                span.classList.add("txt");
+                const image = document.createElement("img");
+                image.classList.add("imgg");
+                span.innerText = element.name;
+                image.src = element.background_image;
+                div.appendChild(image);
+                div.appendChild(span);
+                page.appendChild(div);
+            });
+        })
+        .catch((error) => console.error("Error:", error));
+}
